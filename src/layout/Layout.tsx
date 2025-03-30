7796174a2ad2a53db346df3f645ce70a909f1641
MOJAN <sjs732@naver.com>
2025-03-30 18:01:23 +0900

    Feat: Layout 설정

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,8 +2,8 @@ import { HTMLAttributes } from "react";
 
 const Layout = ({ children }: HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div className="bg-neutral-200">
-      <div>{children}</div>
+    <div className="flex flex-col items-center min-h-screen bg-neutral-100">
+      <div className="flex-grow min-w-[512px] bg-white">{children}</div>
     </div>
   );
 };
