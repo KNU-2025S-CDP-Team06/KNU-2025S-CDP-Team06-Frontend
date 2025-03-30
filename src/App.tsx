ea60959612483bd68207f55413fa69f46a663935
MOJAN <sjs732@naver.com>
2025-03-30 18:01:04 +0900

    Feat: Icon, Pretendard 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Layout from "@layout/Layout";
+import Onboarding from "./pages/onboarding/Onboarding";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Layout>Hi</Layout>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Onboarding />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
---
+++ src/components/Icons.tsx
@@ -0,0 +1,75 @@
+import { HTMLAttributes } from "react";
+
+export const MoveIcon = ({ ...props }: HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      {...props}
+      className={"icon-[icon-park--to-right] " + (props.className ?? "")}
+    />
+  );
+};
+
+export const HomeIcon = ({ ...props }: HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      {...props}
+      className={
+        "icon-[material-symbols--home-outline] " + (props.className ?? "")
+      }
+    />
+  );
+};
+
+export const ReportIcon = ({ ...props }: HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      {...props}
+      className={"icon-[carbon--report] " + (props.className ?? "")}
+    />
+  );
+};
+
+export const MyPageIcon = ({ ...props }: HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      {...props}
+      className={"icon-[material-symbols--person] " + (props.className ?? "")}
+    />
+  );
+};
+
+export const DownIcon = ({ ...props }: HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      {...props}
+      className={"icon-[mingcute--down-line] " + (props.className ?? "")}
+    />
+  );
+};
+
+export const LeftIcon = ({ ...props }: HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      {...props}
+      className={"icon-[mingcute--left-line] " + (props.className ?? "")}
+    />
+  );
+};
+
+export const IncreaseIcon = ({ ...props }: HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      {...props}
+      className={"icon-[icon-park-solid--up-two] " + (props.className ?? "")}
+    />
+  );
+};
+
+export const DecreaseIcon = ({ ...props }: HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      {...props}
+      className={"icon-[icon-park-solid--down-two] " + (props.className ?? "")}
+    />
+  );
+};
