9f2940053ac6c5d02ae4811f027a6db9dc8f70e2
heydylee <sub09065@gmail.com>
2025-04-01 03:04:22 +0900

    Feat: 'mypage' path추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import Layout from "@layout/Layout";
 import OnBoarding from "@pages/onboarding/OnBoarding";
 import Login from "@pages/login/Login";
-
+import Mypage from "@pages/mypage/MyPage";
 const App = () => {
   return (
     <BrowserRouter>
@@ -11,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<OnBoarding />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/mypage" element={<Mypage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
