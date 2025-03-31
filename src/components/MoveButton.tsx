c8496442aced38215e6a0f0714fd6439e66cf1b7
heydylee <sub09065@gmail.com>
2025-04-01 03:03:43 +0900

    Edit: css 수정

--- src/components/MoveButton.tsx
+++ src/components/MoveButton.tsx
@@ -5,11 +5,11 @@ const MoveButton = ({ ...props }: HTMLAttributes<HTMLButtonElement>) => {
     <button
       {...props}
       className={
-        "cursor-pointer flex px-3 py-2 gap-2 text-base font-medium text items-center justify-center bg-white border border-neutral-200 rounded-lg text-neutral-500" +
+        "cursor-pointer flex px-3 py-2 gap-2 text-base font-medium text-neutral-500 items-center justify-center bg-white border border-neutral-200 rounded-lg " +
         (props.className ?? "")
       }
     >
-      <MoveIcon className="w-4 h-4 color-" />
+      <MoveIcon className="w-4 h-4" />
       {props.children}
     </button>
   );
