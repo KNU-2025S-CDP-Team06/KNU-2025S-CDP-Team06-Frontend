34d0a2ce69f174c6fcd19fafd347347423e78559
MOJAN <sjs732@naver.com>
2025-03-31 23:00:49 +0900

    Fix: Layout css 변경

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { HTMLAttributes } from "react";
 const Layout = ({ children }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div className="flex flex-col items-center min-h-screen bg-neutral-100">
-      <div className="flex-grow min-w-[512px] bg-white">{children}</div>
+      <div className="flex-grow max-w-[512px] w-full bg-white">{children}</div>
     </div>
   );
 };
