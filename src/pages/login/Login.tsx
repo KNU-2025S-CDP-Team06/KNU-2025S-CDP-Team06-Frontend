31340e63ad48986f107c6d9fc7a29e7b5794eec9
MOJAN <sjs732@naver.com>
2025-04-01 01:21:12 +0900

    Feat: Login에서 로고 클릭시 메인으로 이동하도록 변경

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,18 @@
 import Button from "@components/Button";
 import MokiLogo from "@components/MokiLogo";
 import TextInput from "@components/TextInput";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
     <main className="flex flex-col items-center gap-32 px-8 pt-32">
-      <MokiLogo className="w-40" />
+      <MokiLogo
+        onClick={() => {
+          navigate("/");
+        }}
+        className="w-40"
+      />
       <section className="flex flex-col w-full gap-4">
         <div className="flex flex-col gap-1">
           <p className="text-sm font-semibold">아이디</p>
