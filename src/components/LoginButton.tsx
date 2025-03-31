4d445e7a90948b10ef6a1a5e8ee38988c4c2e502
MOJAN <sjs732@naver.com>
2025-03-31 23:01:29 +0900

    Fix: Login Button classname null일때 undefined 안나오도록 변경, cursor-pointer 추가

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,8 +4,8 @@ const LoginButton = ({ ...props }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div
       className={
-        "flex p-3 text-lg font-bold items-center justify-center text-white bg-black rounded-2xl " +
-        props.className
+        "cursor-pointer flex p-3 text-lg font-bold items-center justify-center text-white bg-black rounded-2xl " +
+        (props.className ?? "")
       }
     >
       {props.children}
