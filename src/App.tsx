b8f0f37b6aa5fb9f4fa186cbf2638884956c1fde
MOJAN <sjs732@naver.com>
2025-04-02 11:45:14 +0900

    Feat: Main 추가 및 Login 기능 임시 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,33 @@ import Layout from "@layout/Layout";
 import OnBoarding from "@pages/onboarding/OnBoarding";
 import Login from "@pages/login/Login";
 import Mypage from "@pages/mypage/MyPage";
+import { useEffect, useState } from "react";
+import Main from "@pages/main/Main";
+import Nav from "@components/Nav";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<OnBoarding />} />
+          <Route path="/" element={<MainRoute />} />
           <Route path="/login" element={<Login />} />
           <Route path="/mypage" element={<Mypage />} />
         </Routes>
+        <Nav />
       </Layout>
     </BrowserRouter>
   );
+};
+
+const MainRoute = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    setIsAuthenticated(!!token);
+  }, []);
+
+  return isAuthenticated ? <Main></Main> : <OnBoarding />;
 };
 
 export default App;
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,29 +1,64 @@
 import Button from "@components/Button";
 import MokiLogo from "@components/MokiLogo";
 import TextInput from "@components/TextInput";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  interface Auth {
+    id: string;
+    password: string;
+  }
+
+  const [auth, setAuth] = useState<Auth>({ id: "", password: "" });
+  const setToken = () => {
+    sessionStorage.setItem("token", auth.id);
+  };
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const isLogined = !!sessionStorage.getItem("token");
+    if (isLogined) navigate("/");
+  }, []);
   return (
     <main className="flex flex-col items-center gap-32 px-8 pt-32">
       <MokiLogo
         onClick={() => {
           navigate("/");
         }}
-        className="w-40"
+        className="w-40 cursor-pointer"
       />
       <section className="flex flex-col w-full gap-4">
         <div className="flex flex-col gap-1">
           <p className="text-sm font-semibold">아이디</p>
-          <TextInput placeholder="사업자 번호" />
+          <TextInput
+            placeholder="사업자 번호"
+            onChange={(e) => {
+              setAuth({ ...auth, id: e.currentTarget.value });
+            }}
+          />
         </div>
         <div className="flex flex-col gap-1">
           <h2 className="text-sm font-semibold">비밀번호</h2>
-          <TextInput placeholder="계정 비밀번호" type="password" />
+          <TextInput
+            placeholder="계정 비밀번호"
+            type="password"
+            onChange={(e) => {
+              setAuth({ ...auth, password: e.currentTarget.value });
+            }}
+          />
         </div>
         <div className="pt-4">
-          <Button className="w-full">로그인</Button>
+          <Button
+            className="w-full"
+            onClick={() => {
+              setToken();
+              navigate("/");
+            }}
+          >
+            로그인
+          </Button>
         </div>
       </section>
     </main>
