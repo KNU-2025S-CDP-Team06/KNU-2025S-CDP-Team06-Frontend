6588ee2eeec03c21e9fc3229de4bc8ea67e79468
MOJAN <sjs732@naver.com>
2025-04-24 03:50:21 +0900

    Feat: Line Graph 값 변경 기능 추가

--- src/components/graph/LineGraph.tsx
+++ src/components/graph/LineGraph.tsx
@@ -13,34 +13,40 @@ const LineGraph = ({
   data: DailySales[];
   plotBy?: "total_revenue" | "total_count";
 }) => {
-  const [graphData, setGraphData] = useState<Serie[]>([]);
+  const [graphData, setGraphData] = useState<Serie[]>([{ id: "", data: [] }]);
   const [max, setMax] = useState<number>(0);
+  const [leftpad, setLeftpad] = useState<number>(0);
+
+  const [plot, setPlot] = useState<number>(1);
 
   useEffect(() => {
-    setGraphData([
-      {
-        id: "data",
-        data: data.map((dailyData) => {
-          return {
-            y: dailyData[plotBy],
-            x: `${dailyData.date.toISOString().slice(5, 10)}`,
-          };
-        }),
-      },
-    ]);
+    if (plot != 0) {
+      setGraphData([
+        {
+          id: "data",
+          data: data.slice(plot == 1 ? -7 : -31).map((dailyData) => {
+            return {
+              y: dailyData[plotBy],
+              x: `${dailyData.date.toISOString().slice(5, 10)}`,
+            };
+          }),
+        },
+      ]);
+    }
     const maxVal = data.reduce((max, curr) => {
       return Math.max(curr[plotBy], max);
     }, 0);
     const maxStep = Math.pow(10, Math.floor(Math.log10(maxVal)));
+    setLeftpad(Math.floor(Math.log10(maxVal)) * 10);
     setMax(maxStep * Math.ceil(maxVal / maxStep));
-  }, []);
+  }, [plot]);
 
   return (
-    <section className="flex flex-col gap-4 p-4 overflow-hidden rounded-2xl bg-neutral-100">
+    <section className="flex flex-col gap-4 p-4 overflow-visible rounded-2xl bg-neutral-100">
       <div className="w-full h-[300px]">
         <ResponsiveLine
           data={graphData}
-          margin={{ top: 20, right: 15, bottom: 30, left: 50 }}
+          margin={{ top: 20, right: 20, bottom: 30, left: leftpad }}
           xScale={{ type: "point" }}
           yScale={{
             type: "linear",
@@ -53,11 +59,15 @@ const LineGraph = ({
           axisLeft={{
             format: ">-,.0r",
             tickSize: 5,
-            tickValues: 5,
             tickPadding: 5,
             legendOffset: -40,
             legendPosition: "middle",
             truncateTickAt: 0,
+          }}
+          axisBottom={{
+            tickValues: graphData[0].data
+              .map((d) => d.x)
+              .filter((_, i) => i % (plot == 1 ? 1 : 5) === 0),
           }}
           defs={[
             linearGradientDef("gradient", [
@@ -78,9 +88,9 @@ const LineGraph = ({
             return (
               <div
                 style={{ transform: "translateY(3px)" }}
-                className="px-1 text-sm font-semibold bg-white rounded-md shadow-md"
+                className="px-2 py-1 text-xs font-bold bg-white border rounded-sm shadow-md border-neutral-300"
               >
-                {point.point.data.y.toLocaleString()}
+                ₩{point.point.data.y.toLocaleString()}
               </div>
             );
           }}
@@ -91,9 +101,27 @@ const LineGraph = ({
         />
       </div>
       <div className="flex gap-2 px-8">
-        <SmallButton className="flex-grow">1일</SmallButton>
-        <SmallButton className="flex-grow">1주일</SmallButton>
-        <SmallButton className="flex-grow">1개월</SmallButton>
+        <SmallButton
+          isSecondary={plot != 0}
+          onClick={() => setPlot(0)}
+          className="flex-grow"
+        >
+          1일
+        </SmallButton>
+        <SmallButton
+          isSecondary={plot != 1}
+          onClick={() => setPlot(1)}
+          className="flex-grow"
+        >
+          1주일
+        </SmallButton>
+        <SmallButton
+          isSecondary={plot != 2}
+          onClick={() => setPlot(2)}
+          className="flex-grow"
+        >
+          1개월
+        </SmallButton>
       </div>
     </section>
   );
