7a74f4cb1d79e274c61874b01cca0d7e5df479b1
MOJAN <sjs732@naver.com>
2025-05-12 01:35:53 +0900

    Design: 디자인 수정

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -3,7 +3,7 @@ import MoveButton from "@components/ui/MoveButton";
 
 const Mypage = () => {
   return (
-    <main className="flex flex-col items-center gap-32 px-8 pt-32">
+    <main className="flex flex-col items-center">
       <section className="flex flex-col w-full gap-4 px-4 py-3">
         <div>
           <p className="text-xl font-semibold">해머스미스커피 서여의도점</p>
