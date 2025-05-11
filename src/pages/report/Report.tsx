b9df77a8a27156829faa210c6b4ba6d62a6992b0
heydylee <sub09065@gmail.com>
2025-05-11 12:26:29 +0900

    Feat : Report 페이지 Article 추가

--- src/pages/report/Report.tsx
+++ src/pages/report/Report.tsx
@@ -1,22 +1,12 @@
-import ArticleThumbnail from "@components/ui/ArticleThumbnail";
 import ArticleBestmenu from "@components/report/ArticleBestmenu";
+import ArticleSales from "@components/report/ArticleSales";
+import ArticleMonthly from "@components/report/ArticleMonthly";
 const Report = () => {
   return (
-    <main className="flex flex-col items-center justify-between flex-grow px-8 pt-32 pb-8">
-      <ArticleThumbnail title="실시간 매출 리포트" className="w-full bg-white ">
-        <div className="flex-col w-full"></div>
-      </ArticleThumbnail>
+    <main className="flex flex-col items-center justify-between flex-grow px-4 py-3 gap-4">
+      <ArticleSales></ArticleSales>
+      <ArticleMonthly></ArticleMonthly>
       <ArticleBestmenu></ArticleBestmenu>
-
-      <ArticleThumbnail
-        title="내일 예상 매출 리포트"
-        className="w-full bg-white "
-      >
-        <div className="flex-col w-full"></div>
-      </ArticleThumbnail>
-      <ArticleThumbnail title="월간 매출 리포트" className="w-full bg-white ">
-        <div className="flex-col w-full"></div>
-      </ArticleThumbnail>
     </main>
   );
 };
