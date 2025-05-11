523822ee37e025f1c5539f9d83b6f95cd8d1212e
heydylee <sub09065@gmail.com>
2025-05-11 22:26:37 +0900

    Feat : Report 페이지 컴포넌트 추가

--- src/pages/report/Report.tsx
+++ src/pages/report/Report.tsx
@@ -1,12 +1,14 @@
 import ArticleBestmenu from "@components/report/ArticleBestmenu";
 import ArticleSales from "@components/report/ArticleSales";
 import ArticleMonthly from "@components/report/ArticleMonthly";
+import ArticlePredict from "@components/report/ArticlePredict";
 const Report = () => {
   return (
     <main className="flex flex-col items-center justify-between flex-grow px-4 py-3 gap-4">
       <ArticleSales></ArticleSales>
       <ArticleMonthly></ArticleMonthly>
       <ArticleBestmenu></ArticleBestmenu>
+      <ArticlePredict></ArticlePredict>
     </main>
   );
 };
