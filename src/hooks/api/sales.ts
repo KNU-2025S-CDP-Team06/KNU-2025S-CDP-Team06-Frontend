a8d3b09e7afceed487dde7507aefa7a38f6b290b
MOJAN <sjs732@naver.com>
2025-05-13 23:56:52 +0900

    Edit: useQuery Key 추가

--- src/hooks/api/sales.ts
+++ src/hooks/api/sales.ts
@@ -10,21 +10,21 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useGetOneDaySales = (params: getDailySalesParams) => {
   return useQuery({
-    queryKey: ["dailySales"],
+    queryKey: ["dailySales", params.date],
     queryFn: () => getOneDaySales(params),
   });
 };
 
 export const useGetSales = (params: getSalesParams) => {
   return useQuery({
-    queryKey: ["sales"],
+    queryKey: ["sales", params.startDate + params.endDate],
     queryFn: () => getSales(params),
   });
 };
 
 export const useGetTotalSales = (params: getTotalSalesParams) => {
   return useQuery({
-    queryKey: ["totalSales"],
+    queryKey: ["totalSales", params.startDate + params.endDate],
     queryFn: () => getTotalSales(params),
   });
 };
