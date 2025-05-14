76b0cfc0b1c9a0322c0e839b63320497d0089f4d
heydylee <sub09065@gmail.com>
2025-05-14 17:20:14 +0900

    Feat: 시간 호출 적용

--- src/pages/report/Menu/Menu.tsx
+++ src/pages/report/Menu/Menu.tsx
@@ -1,33 +1,33 @@
 import PieGraph from "@components/graph/PieGraph";
 import { useGetOneDaySales } from "@/hooks/api/sales";
-import { useGetSales } from "@/hooks/api/sales";
 import MenuList from "@components/ui/MenuList";
 import Skeleton from "@components/ui/Skeleton";
+import { getThisDay } from "@/utils/day";
+
 const Menu = () => {
+  const today = getThisDay();
+
   const { data: onedayData, isLoading: isOnedayDataLoading } =
-    useGetOneDaySales({ date: "2025-03-26" });
-  const { data: menuList, isLoading: isMenuLoading } = useGetSales({
-    startDate: "2025-03-01",
-    endDate: "2025-04-09",
-  });
+    useGetOneDaySales({ date: today.format("YYYY-MM-DD") });
+
   return (
     <div className="flex flex-col gap-4 px-4 py-3">
       <div className="flex items-center justify-center"></div>
       {isOnedayDataLoading ? (
-        <Skeleton height={516} />
+        <Skeleton height={517} />
       ) : (
         <PieGraph data={onedayData!.sales_data} />
       )}
       <div className="h-[1px] bg-neutral-300 w-full" />
 
       <div>
-        {isMenuLoading ? (
+        {isOnedayDataLoading ? (
           <Skeleton height={516} />
         ) : (
           <MenuList
             title="오늘 가장 많이 판매된 메뉴"
-            data={menuList!.slice(-1)[0].sales_data}
-            prevData={menuList!.slice(-2, -1)[0].sales_data}
+            data={onedayData!.sales_data}
+            prevData={onedayData!.sales_data}
           />
         )}
       </div>
--- src/pages/report/Report.tsx
+++ src/pages/report/Report.tsx
@@ -2,12 +2,22 @@ import ArticleMenu from "@components/report/ArticleMenu";
 import ArticleSales from "@components/report/ArticleSales";
 import ArticleMonthly from "@components/report/ArticleMonthly";
 import ArticlePredict from "@components/report/ArticlePredict";
+import { useEffect, useState } from "react";
+import { getThisDay } from "@/utils/day";
 const Report = () => {
+  const today = getThisDay();
+
+  const [timestamp, setTimestamp] = useState<string>("");
+
+  useEffect(() => {
+    setTimestamp(today.format("MM월 DD일"));
+  }, []);
+
   return (
     <main className="flex flex-col items-center justify-between flex-grow gap-4 px-4 py-3">
       <div className="flex items-center w-full gap-2">
         <div className="h-0.5 bg-neutral-200 flex-grow" />
-        <h1 className="text-lg font-semibold">3월 27일</h1>
+        <h1 className="text-lg font-semibold">{timestamp}</h1>
         <div className="h-0.5 bg-neutral-200 flex-grow" />
       </div>
       <div className="flex flex-col w-full gap-4 px-3">
