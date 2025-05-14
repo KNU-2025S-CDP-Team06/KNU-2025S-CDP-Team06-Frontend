1bba53a56ff51705c0fc4462ed8aa248c8d20e80
heydylee <sub09065@gmail.com>
2025-05-15 01:23:19 +0900

    Feat : 메인 페이지 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,81 @@
+import BarGraph, { BarGraphData } from "@components/graph/BarGraph";
+import { useGetSales } from "@/hooks/api/sales";
+import { useGetOneDayPredict } from "@/hooks/api/predict";
+import { useEffect, useState } from "react";
+import BothsideText from "@components/ui/BothsideText";
+import { useGetStoreData } from "@/hooks/api/storeData";
+import { useGetTotalSales } from "@/hooks/api/sales";
+import SalesReport from "@components/main/SalesReport";
+import TodayBestMenu from "@components/main/TodayBestMenu";
+import DailyPredict from "@components/main/DailyPredict";
 const Main = () => {
+  const { data: predictData, isLoading: isPredictDataLoading } =
+    useGetOneDayPredict("2025-04-10");
+
+  const { data: manydayData, isLoading: isManydayDataLoading } = useGetSales({
+    startDate: "2025-03-01",
+    endDate: "2025-04-09",
+  });
+
+  const { data: totalSales, isLoading: isTotalSalesLoading } = useGetTotalSales(
+    {
+      startDate: "2025-03-01",
+      endDate: "2025-04-01",
+    }
+  );
+  const [barGraphData, setBarGraphData] = useState<BarGraphData[]>([]);
+
+  const { data: storeData, isLoading: isStoreLoading } = useGetStoreData({
+    id: 1,
+  });
+
+  useEffect(() => {
+    if (!isPredictDataLoading && !isManydayDataLoading) {
+      const graphData: BarGraphData[] = [];
+      manydayData!.slice(-4).forEach((data) => {
+        const revenue = Math.floor(data.total_revenue / 10000);
+        graphData.push({
+          data: revenue,
+          title: `${revenue}만원`,
+          paragraph: `${data.date.slice(5)}`,
+        });
+      });
+      graphData.push({
+        data: Math.floor(predictData!.xgboost_forecast / 10000),
+        title: `${Math.floor(predictData!.xgboost_forecast / 10000)}만원`,
+        paragraph: "04-10",
+        ispredict: true,
+      });
+      setBarGraphData(graphData);
+    }
+  }, [isPredictDataLoading, isManydayDataLoading]);
+
   return (
-    <div className="flex flex-col gap-4 px-4 py-3">
-      <h1 className="text-xl font-semibold text-center">실시간 매출 리포트</h1>
+    <div className="flex flex-col gap-3 p-4">
+      <SalesReport></SalesReport>
+      <div className="h-[1px] bg-neutral-300 w-full" />
+      <TodayBestMenu></TodayBestMenu>
+      <div className="h-[1px] bg-neutral-300 w-full" />
+      <DailyPredict></DailyPredict>
+      <div className="h-[1px] bg-neutral-300 w-full" />
+
+      {isPredictDataLoading || isManydayDataLoading ? (
+        <>스켈레톤</>
+      ) : (
+        <BarGraph data={barGraphData} />
+      )}
+
+      {isStoreLoading ? (
+        <>스켈레톤</>
+      ) : (
+        <BothsideText label={storeData!.name} value={storeData!.address} />
+      )}
+      {isTotalSalesLoading ? (
+        <>스켈레톤</>
+      ) : (
+        <BothsideText label="이번달 매출" value={totalSales!.total_revenue} />
+      )}
     </div>
   );
 };
-
 export default Main;
