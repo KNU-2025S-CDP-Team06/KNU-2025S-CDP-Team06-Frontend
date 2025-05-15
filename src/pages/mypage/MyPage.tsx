e883f11758e01dffac255f419144dcf907981a4d
MOJAN <sjs732@naver.com>
2025-05-15 13:53:18 +0900

    Feat: ID로 API 불러오는 기능 추가

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -2,14 +2,15 @@ import LoginButton from "@components/ui/LoginButton";
 import MoveButton from "@components/ui/MoveButton";
 import { useGetStoreData } from "@/hooks/api/storeData";
 const Mypage = () => {
+  const id = sessionStorage.getItem("token");
   const { data: storeData, isLoading: isStoreLoading } = useGetStoreData({
-    id: 1,
+    id: id!,
   });
 
   return (
     <main className="flex flex-col items-center ">
       <section className="flex flex-col w-full gap-4 px-4 py-3">
-        <div className="text-xl text-pretty font-semibold">
+        <div className="text-xl font-semibold text-pretty">
           {isStoreLoading ? <>스켈레톤</> : storeData!.name}
           <p className="text-base font-normal text-neutral-400">
             {isStoreLoading ? <>스켈레톤</> : storeData!.address}
