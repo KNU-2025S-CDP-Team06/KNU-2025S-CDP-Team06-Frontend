e4638366e2fcf9c2531727fc9de4b877de18a82a
MOJAN <sjs732@naver.com>
2025-05-15 13:53:01 +0900

    Design: NavBar가 있을시 하단 margin 추가

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -13,47 +13,50 @@ const Nav = () => {
   }, [pathname]);
   return (
     isLogined && (
-      <nav
-        className="fixed bottom-0 w-11/12 max-w-[455px] flex px-12 py-2 justify-between rounded-t-2xl bg-white border-t border-r border-l border-neutral-300 text-neutral-300 font-bold drop-shadow-[0_1px_16px_rgba(0,0,0,0.25)]"
-        style={{ left: "50%", transform: "translate(-50%, 0)" }}
-      >
-        <div
-          onClick={() => {
-            navigate("/");
-          }}
-          className={
-            "flex flex-col items-center w-12 cursor-pointer" +
-            (rootPath == "" ? " text-blue-400" : "")
-          }
+      <>
+        <div className="mt-16" />
+        <nav
+          className="fixed bottom-0 w-11/12 max-w-[455px] flex px-12 py-2 justify-between rounded-t-2xl bg-white border-t border-r border-l border-neutral-300 text-neutral-300 font-bold drop-shadow-[0_1px_16px_rgba(0,0,0,0.25)]"
+          style={{ left: "50%", transform: "translate(-50%, 0)" }}
         >
-          <HomeIcon className="w-6 h-6" />
-          <span className="text-[10px] font-bold">메인화면</span>
-        </div>
-        <div
-          onClick={() => {
-            navigate("/report");
-          }}
-          className={
-            "flex flex-col items-center w-12 cursor-pointer" +
-            (rootPath == "report" ? " text-blue-400" : "")
-          }
-        >
-          <ReportIcon className="w-6 h-6" />
-          <span className="text-[10px] font-bold">리포트</span>
-        </div>
-        <div
-          onClick={() => {
-            navigate("/mypage");
-          }}
-          className={
-            "flex flex-col items-center w-12 cursor-pointer" +
-            (rootPath == "mypage" ? " text-blue-400" : "")
-          }
-        >
-          <MyPageIcon className="w-6 h-6" />
-          <span className="text-[10px] font-bold">마이페이지</span>
-        </div>
-      </nav>
+          <div
+            onClick={() => {
+              navigate("/");
+            }}
+            className={
+              "flex flex-col items-center w-12 cursor-pointer" +
+              (rootPath == "" ? " text-blue-400" : "")
+            }
+          >
+            <HomeIcon className="w-6 h-6" />
+            <span className="text-[10px] font-bold">메인화면</span>
+          </div>
+          <div
+            onClick={() => {
+              navigate("/report");
+            }}
+            className={
+              "flex flex-col items-center w-12 cursor-pointer" +
+              (rootPath == "report" ? " text-blue-400" : "")
+            }
+          >
+            <ReportIcon className="w-6 h-6" />
+            <span className="text-[10px] font-bold">리포트</span>
+          </div>
+          <div
+            onClick={() => {
+              navigate("/mypage");
+            }}
+            className={
+              "flex flex-col items-center w-12 cursor-pointer" +
+              (rootPath == "mypage" ? " text-blue-400" : "")
+            }
+          >
+            <MyPageIcon className="w-6 h-6" />
+            <span className="text-[10px] font-bold">마이페이지</span>
+          </div>
+        </nav>
+      </>
     )
   );
 };
